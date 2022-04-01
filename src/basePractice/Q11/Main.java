f589d86099e91de4422e61663de78d1473aa2882
D <1285434409@qq.com>
2022-04-01 22:20:29 +0800

    十六进制转十进制

--- src/basePractice/Q11/Main.java
+++ src/basePractice/Q11/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 /**
  * 从键盘输入一个不超过8位的正的十六进制数字符串，将它转换为正的十进制数后输出。
  * 注：十六进制数中的10~15分别用大写的英文字母A、B、C、D、E、F表示。
+ * 注意：output越界问题
  */
 public class Main {
     public static void main(String[] args) {
@@ -12,9 +13,9 @@ public class Main {
         String hexString = sc.next();
 
         hexString = new StringBuilder(hexString).reverse().toString();
-        int output = 0;
+        long output = 0;
         for (int i = 0; i < hexString.length(); i++) {
-            int num = 0;
+            int num;
             char hexChar = hexString.charAt(i);
             if (hexChar >= 'A' && hexChar <= 'F') {
                 num = hexChar - 55;
